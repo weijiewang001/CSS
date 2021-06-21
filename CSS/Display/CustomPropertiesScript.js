f978fc1b434b2a2a590afa1ae46f35a959d51968
weijiewang001 <65530009+weijiewang001@users.noreply.github.com>
2021-06-21 23:38:44 +1000

    Custom Properties

---
+++ CSS/Display/CustomPropertiesScript.js
@@ -0,0 +1,12 @@
+// console.log(window.getComputedStyle(doucument.doucumentElement)
+// .getPropertyValue('--div-background-color'))
+
+document.getElementById('dark-theme-btn').addEventListener('click',() => {
+    document.documentElement.style.setProperty
+    ('--background-color', '#333')
+})
+
+document.getElementById('light-theme-btn').addEventListener('click',() => {
+    document.documentElement.style.setProperty
+    ('--background-color', 'white')
+})
